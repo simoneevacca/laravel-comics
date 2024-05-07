6a2f8b54370b2e3c4a9a8236bda1afecbdb2b38d
simoneevacca <v.simone.vacca@gmail.com>
2024-05-07 16:40:29 +0200

    add cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>comics</h1>
-</div>
+<section class="comics">
+    <div class="container">
+        
+        <div class="row row-cols-6">
+            @foreach ($comics_list as $comic) 
+                <div class="col">
+                    <div class="card d-flex flex-column">
+                        <img  src="{{$comic['thumb']}}" alt="image describing the following comic {{$comic['title']}}">
+                        <div class="title d-flex align-items-center">{{$comic['title']}}</div>
+    
+                    </div>
+                </div>
+            @endforeach
+    
+        </div>
+    </div>
+
+</section>
 @endsection
