b285183a1525ebb0f0a5650bf7cc9e078b02bfc1
simoneevacca <v.simone.vacca@gmail.com>
2024-05-07 15:52:27 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,95 @@
 
-<footer>footer</footer>
+<footer>
+    <div class="banner">
+        <div class="container d-flex justify-content-between">
+        
+            <div>
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                <div>DIGITAL COMICS</div>
+            </div>
+            <div>
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                <div>DC MERCHANDISE</div>
+            </div>
+            <div>
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                <div>SUBSCRIPTION</div>
+            </div>
+            <div>
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                <div>COMIC SHOP LOCATOR</div>
+            </div>
+            <div>
+                <i class="fa-regular fa-credit-card"></i>
+                <div>DC POWER VISA</div>
+            </div>
+        </div>
+    </div>
+    <div class="link">
+        <div class="container d-flex">
+            <div class="left d-flex">
+                <div>
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                    </ul>
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>Lorem.</li>
+                        <li>Omnis.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>DC</h4>
+                    <ul>
+                        <li>Lorem.</li>
+                        <li>Assumenda.</li>
+                        <li>Hic.</li>
+                        <li>Quaerat!</li>
+                        <li>Dicta!</li>
+                        <li>Nulla!</li>
+                        <li>Accusantium.</li>
+                        <li>Ratione.</li>
+                        <li>Alias!</li>
+                        <li>Aspernatur.</li>
+                        <li>Vero?</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>Lorem.</li>
+                        <li>Sunt.</li>
+                        <li>Maiores!</li>
+                        <li>Molestiae!</li>
+                        <li>Voluptate.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right"></div>
+        </div>
+    </div>
+    <div class="social">
+        <div class="container d-flex justify-content-between">
+            <div>
+                <button class="btn btn-outline-light">SIGN-UP NOW!</button>
+            </div>
+            <div class="icons d-flex">
+                <h4>FOLLOW US</h4>
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+
+            </div>
+        </div>
+    </div>
+    
+</footer>
